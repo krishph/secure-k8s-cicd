683b32c90428090267962c5a7953edf94ff0bf80
krishph <42297817+krishph@users.noreply.github.com>
2023-01-04 20:04:51 -0500

    ec2 code updated

--- EC2/main.tf
+++ EC2/main.tf
@@ -17,14 +17,6 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  default = "172.16.0.0/16"
-}
-
-variable "subnet1_cidr" {
-  default = "172.16.0.0/24"
-}
-
 # //////////////////////////////
 # PROVIDERS
 # //////////////////////////////
@@ -36,19 +28,117 @@ provider "aws" {
 
 
 # //////////////////////////////
-# MODULES
+# RESOURCES
 # //////////////////////////////
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+# SECURITY_GROUP
+resource "aws_security_group" "my-sg-grp" {
+  name = "ikrish-tf-sg-ec2"
+  vpc_id = var.vpc_id
 
-  name = "vpc-module-example"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  cidr = "10.0.0.0/16"
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+# INSTANCE
+resource "aws_instance" "myec2" {
+  ami = data.aws_ami.aws-linux.id
+  instance_type = "t2.micro"
+  subnet_id = values(data.aws_subnet.example)[1].id
+  vpc_security_group_ids = [aws_security_group.my-sg-grp.id]
+  key_name               = var.ssh_key_name
+
+  connection {
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+  }
+
+  tags = {
+    "Name" = "ikrish-tf-ec2-myec2"
+  }    
+}
+
+# //////////////////////////////
+# DATA
+# //////////////////////////////
+data "aws_vpcs" "vpcs" {}
+
+data "aws_vpc" "allvpcs" {
+  count = length(data.aws_vpcs.vpcs.ids)
+  id    = tolist(data.aws_vpcs.vpcs.ids)[count.index]
+}
+
+#From vpcs id's fetched, use first vpc, assuming there will be only one vpc
+data "aws_subnets" "example" {
+  filter{
+     name = "vpc-id"
+     values = [tolist(data.aws_vpcs.vpcs.ids)[0]]
+  }
+  filter{
+    name = "map-public-ip-on-launch"
+    values = [true]
+  }
+}
+
+data "aws_subnet" "example" {
+  for_each = toset(data.aws_subnets.example.ids)
+  id       = each.value
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "aws-linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+# //////////////////////////////
+# OUTPUT
+# //////////////////////////////
+
+output "instance-dns" {
+  value = aws_instance.myec2.public_dns
+}
+
--- VPC/main.tf
+++ VPC/main.tf
@@ -41,7 +41,7 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "vpc-module-example"
+  name = "ikrish-tf-vpc"
 
   cidr = "10.0.0.0/16"
 
